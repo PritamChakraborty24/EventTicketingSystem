cdfdc93ad6e400c036c979f3374f32f0cda025ee
Pritam Chakraborty <chakraborty.pritam24@gmail.com>
2024-09-14 21:30:57 +0530

    Initial Commit

---
+++ src/test/java/com/bolt/earth/assignment/eventticketingsystem/EventTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolt.earth.assignment.eventticketingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
